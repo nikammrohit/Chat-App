e461c1a823d6b64e1aa2c615767be3ba972a9f8f
nikammrohit <82987914+nikammrohit@users.noreply.github.com>
2024-07-28 01:42:22 -0500

    Connected with Auth Page to authenticate on NodeJS. Additionally Rendered Chats Page and UI and connected it to Chat Engine.

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,4 +1,15 @@
-const ChatsPage = () => {
-    return <div className="background">chats...</div>;
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+    return (
+      <div className="background">
+        <PrettyChatWindow
+          projectId='0b8f53bd-6119-4a71-8de3-f17209091cf6'
+          username={props.user.username}
+          secret={props.user.secret}
+        />
+      </div>
+    );
   };
+  
   export default ChatsPage;
